a1c1ee65a6eae00fa24b02aea759f030612fbe03
Borja Paz Rodríguez <borjapazr@gmail.com>
2021-12-02 22:13:35 +0100

    fix: remove extra spaces

--- challenge-02/index.js
+++ challenge-02/index.js
@@ -2,7 +2,7 @@
 const listGifts = (letter) => {
   return letter
     .split(" ")
-    .filter((word) => word[0] !== "_")
+    .filter((word) => word[0] !== "_" && word)
     .reduce((previous, next) => {
       previous[next] = previous[next] + 1 || 1;
       return previous;
