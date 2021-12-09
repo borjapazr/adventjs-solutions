c05c0bb4e13d3d922d296145c304f7ffbfb58405
Borja Paz Rodríguez <borjapazr@gmail.com>
2021-12-09 23:07:08 +0100

    refactor: move testSequencer to utils folder

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  testSequencer: '<rootDir>/testSequencer.js',
+  testSequencer: '<rootDir>/utils/test-alphabetical-sequencer.js',
   maxWorkers: 1,
   verbose: true,
   setupFiles: ['<rootDir>/utils/test.utils.js']
