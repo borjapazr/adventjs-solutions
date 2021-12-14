8cd444b196f7a7e8c8186846b22125bd185b2491
Borja Paz Rodríguez <borjapazr@gmail.com>
2021-12-14 22:39:02 +0100

    feat: add fourteenth challenge

--- challenge-14/solution.js
+++ challenge-14/solution.js
@@ -1,11 +1,18 @@
 const missingReindeer = ids => {
-  const sumNatural = n => (n * (n + 1)) / 2;
+  ids.sort((a, b) => a - b);
 
-  const maxReindeerId = ids.length;
-  const expectedSum = sumNatural(maxReindeerId);
-  const reindeersIdsSum = ids.reduce((a, b) => a + b, 0);
+  let leftIndex = 0;
+  let rightIndex = ids.length - 1;
+  while (leftIndex <= rightIndex) {
+    const currentIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (ids[currentIndex] === currentIndex) {
+      leftIndex = currentIndex + 1;
+    } else {
+      rightIndex = currentIndex - 1;
+    }
+  }
 
-  return expectedSum - reindeersIdsSum;
+  return leftIndex;
 };
 
 export { missingReindeer };
