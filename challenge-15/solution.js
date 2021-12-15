5819174cd1bed3adcfea755aabaa2cfb07727b5f
Borja Paz Rodríguez <borjapazr@gmail.com>
2021-12-15 19:54:48 +0100

    feat: add fifteenth challenge

--- challenge-15/solution.js
+++ challenge-15/solution.js
@@ -1,6 +1,6 @@
 const checkSledJump = heights => {
   const peakIndex = heights.indexOf(Math.max(...heights));
-  const isPeakAtMiddle = peakIndex !== heights.length - 1;
+  const isPeakAtMiddle = peakIndex !== 0 && peakIndex !== heights.length - 1;
   const isUpAndDown = heights.every((height, index, array) =>
     index < peakIndex ? height < array[index + 1] : height > (array[index + 1] ?? Number.NEGATIVE_INFINITY)
   );
