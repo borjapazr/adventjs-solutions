fe06c2326e5f8c4d15ede5c99ef4a561805b0de6
Borja Paz Rodríguez <borjapazr@gmail.com>
2021-12-15 19:46:25 +0100

    feat: add fifteenth challenge

--- challenge-15/solution.js
+++ challenge-15/solution.js
@@ -2,7 +2,7 @@ const checkSledJump = heights => {
   const peakIndex = heights.indexOf(Math.max(...heights));
   const isPeakAtMiddle = peakIndex !== heights.length - 1;
   const isUpAndDown = heights.every((height, index, array) =>
-    index < peakIndex ? height < array[index + 1] : height > (array[index + 1] || Number.NEGATIVE_INFINITY)
+    index < peakIndex ? height < array[index + 1] : height > (array[index + 1] ?? Number.NEGATIVE_INFINITY)
   );
   return isPeakAtMiddle && isUpAndDown;
 };
--- challenge-15/solution.spec.js
+++ challenge-15/solution.spec.js
@@ -10,7 +10,9 @@ describe('Challenge 15: El salto perfecto', () => {
       createTestCase([[2, 4, 4, 6, 2]], false),
       createTestCase([[1, 2, 3]], false),
       createTestCase([[1, 2, 3, 2, 1, 2, 3]], false),
-      createTestCase([[1, 3]], false)
+      createTestCase([[1, 3]], false),
+      createTestCase([[1]], false),
+      createTestCase([null], false)
     ];
 
     it.each(testCases)('#$# should return $expected when the jump is $args.0', ({ args, expected }) => {
