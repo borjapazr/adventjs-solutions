5d9cc41669e7459e89f6ed93ada09e034a50f288
Borja Paz Rodríguez <borjapazr@gmail.com>
2021-12-18 02:23:51 +0100

    feat: improve challenge 16

--- challenge-16/solution.js
+++ challenge-16/solution.js
@@ -1,7 +1,7 @@
 const decodeNumber = symbols => {
   const dictionary = { '.': 1, ',': 5, ':': 10, ';': 50, '!': 100 };
 
-  return symbols.split('').reduce((number, symbol, index, array) => {
+  return [...symbols].reduce((number, symbol, index, array) => {
     const currentSymbolValue = dictionary[symbol];
     const nextSymbolValue = dictionary[array[index + 1]];
     const operation = currentSymbolValue < nextSymbolValue ? -1 : 1;
