4d25353d69819970e7040bf5cfaaf95a9eb25e6e
Borja Paz Rodríguez <borjapazr@gmail.com>
2021-12-20 01:42:14 +0100

    feat: add nineteenth challenge

--- challenge-19/solution.js
+++ challenge-19/solution.js
@@ -1,3 +1,21 @@
-const learn = (time, courses) => {};
+const learn = (time, courses) => {
+  let candidateCourseIds = null;
+  let maximumAffordableDuration = 0;
+
+  for (let i = 0; i < courses.length; i++) {
+    for (let j = i + 1; j < courses.length; j++) {
+      const coursesDuration = courses[i] + courses[j];
+      if (coursesDuration === time) {
+        return [i, j];
+      }
+      if (coursesDuration < time && coursesDuration > maximumAffordableDuration) {
+        maximumAffordableDuration = coursesDuration;
+        candidateCourseIds = [i, j];
+      }
+    }
+  }
+
+  return candidateCourseIds;
+};
 
 export { learn };
