4212f818d1ac026cb4bcaca2f100f9c1cb24cbd1
Borja Paz Rodríguez <borjapazr@gmail.com>
2021-12-21 01:58:02 +0100

    feat: add twentieth challenge

--- challenge-20/solution.js
+++ challenge-20/solution.js
@@ -3,7 +3,7 @@ const pangram = letter =>
     letter
       .toLowerCase()
       .normalize('NFD')
-      .replace(/^(?!\u0303)[\u0300-\u036F]/g, '')
+      .replace(/([^\u0300-\u036fn]|n(?!\u0303))[\u0300-\u036F]/g, '')
       .replace(/[^\u0303a-z]/g, '')
   ).size === 27;
 
--- challenge-20/solution.spec.js
+++ challenge-20/solution.spec.js
@@ -3,7 +3,7 @@ import { pangram } from './solution';
 describe('Challenge 20: ¿Una carta de pangramas? ¡QUÉ!', () => {
   describe('pangram(...)', () => {
     const testCases = [
-      createTestCase(['Extraño pan de col y kiwi se q´uemó bajo fugaz vaho'], true),
+      createTestCase(['Extraño pan de col y kiwi se quemó bajo fugaz vaho'], true),
       createTestCase(['Jovencillo emponzoñado y con walkman: ¡qué figurota exhibes!'], true),
       createTestCase(['Esto es una frase larga pero no tiene todas las letras del abecedario'], false),
       createTestCase(['De la a a la z, nos faltan letras'], false)
