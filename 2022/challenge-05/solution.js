e4df1643263fef84d16cdeefb2e045c94bc2d94b
Borja Paz Rodríguez <borjapazr@gmail.com>
2023-11-13 22:14:48 +0100

    fix(solution): fix solution of challenge 2022/05

--- 2022/challenge-05/solution.js
+++ 2022/challenge-05/solution.js
@@ -1,4 +1,21 @@
 const getMaxGifts = (giftsCities, maxGifts, maxCities) => {
+  const combinations = giftsCities.reduce(
+    (acc, cityGifts) => acc.concat(acc.map(existingCombination => [cityGifts].concat(existingCombination))),
+    [[]]
+  );
+
+  const validCombinations = combinations.filter(combination => combination.length <= maxCities);
+
+  const sums = validCombinations.map(combination => combination.reduce((total, current) => total + current, 0));
+
+  const validSums = sums.filter(sum => sum <= maxGifts);
+
+  return Math.max(...validSums);
+};
+
+// This solution is faster than the one above but it does not work for some test cases.
+// Form example getMaxGiftsAlt([70, 50, 30], 80, 2) returns 70 but it should return 80
+const getMaxGiftsAlt = (giftsCities, maxGifts, maxCities) => {
   return giftsCities
     .sort((cityGiftsA, cityGiftsB) => cityGiftsB - cityGiftsA)
     .reduce((m, g) => {
